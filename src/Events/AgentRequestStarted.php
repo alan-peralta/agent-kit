2ff01adad059eb64e3b70caa56c553b30eacf11a
Alan Peralta <alan.peralta23@gmail.com>
2026-08-08 15:02:23 -0400

    feat: add agent request lifecycle events

---
+++ src/Events/AgentRequestStarted.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Peralta\AgentKit\Events;
+
+class AgentRequestStarted implements AgentKitEvent
+{
+    public function __construct(
+        public readonly ?string $conversationId,
+        public readonly string $provider,
+        public readonly ?string $model,
+    ) {}
+}
