95376b3159072e5a2448c64d23567efb0e730922
Alan Peralta <alan.peralta23@gmail.com>
2026-08-08 13:19:10 -0400

    fix: preserve exception chain and record original provider failure in FallbackMiddleware

--- src/Exceptions/AllProvidersFailedException.php
+++ src/Exceptions/AllProvidersFailedException.php
@@ -2,12 +2,15 @@
 
 namespace Peralta\AgentKit\Exceptions;
 
+use Throwable;
+
 class AllProvidersFailedException extends AgentException
 {
     public function __construct(
         string $message,
         public readonly array $providerSummary = [],
+        ?Throwable $previous = null,
     ) {
-        parent::__construct($message);
+        parent::__construct($message, 0, $previous);
     }
 }
